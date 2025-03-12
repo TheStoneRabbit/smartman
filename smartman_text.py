c92907cd65e85eddd06f06601fad24cadc278c0f
Mason Lapine <mlapine@360privacy.io>
2025-03-12 14:13:01 -0500

    add upload files

--- smartman_text.py
+++ smartman_text.py
@@ -6,6 +6,8 @@ from rich.console import Console
 from rich.markdown import Markdown
 from rich.syntax import Syntax
 from termcolor import colored
+from PyQt5.QtWidgets import QApplication, QFileDialog, QMainWindow
+
 import pyperclip
 
 import readline
@@ -16,7 +18,7 @@ conversation_log = [{"role": "system", "content": "You are ChatGPT, a helpful as
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 console = Console(color_system="256", emoji=True)
-
+app = QApplication([])
 def spinner():
     while True:
         for cursor in '|/-\\':
@@ -28,7 +30,9 @@ def show_spinner(spinner_gen):
         time.sleep(0.1)
     print("\r" + " " * 15, end="\r")
 
-
+def open_multiple_files():
+    files, _ = QFileDialog.getOpenFileNames(None, "Select Files")
+    return files
 
 def query_chatgpt(prompt):
     global spinner_done
@@ -46,7 +50,7 @@ def query_chatgpt(prompt):
     return chat_completion.choices[0].message.content
 
 if __name__ == "__main__":
-
+    has_files = False
     while True:
         try:
             os.system("clear")
@@ -64,11 +68,28 @@ if __name__ == "__main__":
                 continue
             elif prompt == "/quit":
                 break
+            elif prompt == "/upload":
+                has_files = True
+                selected_files = open_multiple_files()
+                console.clear()
+                print("Files uploaded. Press the enter key to continue.")
+                prompt = input("> ")
             else:
-                response = query_chatgpt(prompt)
+                if has_files:
+                    start_text = "Below are the files you need to analyze."
+                    for file in selected_files:
+                        with open(file, "r") as f:
+                            data = f.read()
+                            name = file.split("/")[-1]
+                            start_text += f"\n{name}: \n\n```\n{data}\n```\n\n"
+                    response = query_chatgpt(f'{prompt}\n\n{start_text}')
+                    has_files = False
+                else:
+                    response = query_chatgpt(prompt)
                 markdown = response
                 text_to_copy = ""
                 count = 0
+
                 for text_portion in range(1, len(markdown.split("```"))):
                     if(text_portion % 2 == 1):
                         count += 1
@@ -83,6 +104,6 @@ if __name__ == "__main__":
                     console.print(syntax)
         except KeyboardInterrupt:
             break
-    print("\n\nGoodbye!")
+    print("\nGoodbye!")
 
 
